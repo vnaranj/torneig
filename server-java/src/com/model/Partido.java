adf3db0734b3ed3d1a6abd44409dac0724781289
vnaranj <129057948+vnaranj@users.noreply.github.com>
2023-04-21 10:40:08 +0000

    Servicio obtener partidos ok

---
+++ server-java/src/com/model/Partido.java
@@ -0,0 +1,99 @@
+package com.model;
+
+import java.util.Date;
+
+public class Partido {
+    private String id;
+    private String idLocal;
+    private String idVisitante;
+    private String equipoLocal;
+    private String equipoVisitante;
+    private Date fecha;
+    private int golesLocal;
+    private int golesVisitante;
+
+    public Partido(String id, String idLocal, String idVisitante, String equipoLocal, String equipoVisitante,
+            Date fecha, int golesLocal,
+            int golesVisitante) {
+        this.id = id;
+        this.idLocal = idLocal;
+        this.idVisitante = idVisitante;
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.fecha = fecha;
+        this.golesLocal = golesLocal;
+        this.golesVisitante = golesVisitante;
+    }
+
+    public Partido(String id, String idLocal, String idVisitante, String equipoLocal, String equipoVisitante,
+            Date fecha) {
+
+        this(id, idLocal, idVisitante, equipoLocal, equipoVisitante, fecha, 0, 0);
+    }
+
+    // getters y setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdLocal() {
+        return idLocal;
+    }
+
+    public void setIdLocal(String idLocal) {
+        this.idLocal = idLocal;
+    }
+
+    public String getIdVisitante() {
+        return idVisitante;
+    }
+
+    public void setIdVisitante(String idVisitante) {
+        this.idVisitante = idVisitante;
+    }
+
+    public String getEquipoLocal() {
+        return equipoLocal;
+    }
+
+    public void setEquipoLocal(String equipoLocal) {
+        this.equipoLocal = equipoLocal;
+    }
+
+    public String getEquipoVisitante() {
+        return equipoVisitante;
+    }
+
+    public void setEquipoVisitante(String equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+}
--- server-java/src/com/partidos/PartidosServlet.java
+++ server-java/src/com/partidos/PartidosServlet.java
@@ -2,8 +2,18 @@ package com.partidos;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.model.Partido;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -14,22 +24,67 @@ public class PartidosServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
+    public PartidosServlet() {
+        super();
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<String> partidos = new ArrayList<String>();
-        partidos.add("Barcelona - Real Madrid");
-        partidos.add("Sevilla - Valencia");
-        partidos.add("Atletico de Madrid - Athletic Club");
-        partidos.add("Real Sociedad - Betis");
-        partidos.add("Villareal - Levante");
 
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
-        out.println("[");
-        for (int i = 0; i < partidos.size(); i++) {
-            out.println("{\"partido\":\"" + partidos.get(i) + "\"}" + (i != partidos.size() - 1 ? "," : ""));
+        // Establece la conexión con la base de datos
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://db:3306/mydatabase";
+            conn = DriverManager.getConnection(url, "root", "mypassword");
+
+            // Ejecuta la consulta y almacena los resultados en una lista de objetos Partido
+            List<Partido> partidos = new ArrayList<>();
+            stmt = conn.createStatement();
+            String sql = "SELECT  p.id, p.hora, \n" +
+                    "       eq1.id AS equipo1_id, eq1.nombre AS equipo1_nombre, \n" +
+                    "       eq2.id AS equipo2_id, eq2.nombre AS equipo2_nombre, \n" +
+                    "       p.equipo1_goles, p.equipo2_goles \n" +
+                    "   FROM partidos p \n" +
+                    "   JOIN equipos eq1 ON p.equipo1_id = eq1.id \n" +
+                    "   JOIN equipos eq2 ON p.equipo2_id = eq2.id;";
+
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String equipo1Id = rs.getString("equipo1_id");
+                String equipo2Id = rs.getString("equipo2_id");
+                String equipo1 = rs.getString("equipo1_nombre");
+                String equipo2 = rs.getString("equipo2_nombre");
+                String fecha = rs.getString("hora");
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Partido partido = new Partido(id, equipo1Id, equipo2Id, equipo1, equipo2, sdf.parse(fecha));
+                partidos.add(partido);
+            }
+
+            // Convierte la lista de objetos Partido a formato JSON y la envía como
+            // respuesta al cliente
+            Gson gson = new Gson();
+            String json = gson.toJson(partidos);
+            response.setContentType("application/json");
+            PrintWriter out = response.getWriter();
+            out.print(json);
+            out.flush();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException e) { // si no se formatea bien la fecha
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        out.println("]");
     }
+
 }
