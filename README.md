bcfae6eb9bf38b5767f864e8377579dbeabcb8f4
vnaranj <129057948+vnaranj@users.noreply.github.com>
2023-04-23 18:59:23 +0000

    Con cards

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Torneig
 Aplicació per a la gestió del torneig de fi de curs de l'institut Guineueta
+
+Instal·lar extensions de Docker i de Java
--- client/js/main.js
+++ client/js/main.js
@@ -1,28 +1,45 @@
-const resultadosTable = document.getElementById("resultados");
+//div para poder meter el html dinámico
+const resultadosDiv = document.getElementById("resultados");
+
+//Botón para refrescar resultados
 const refrescarBtn = document.getElementById("refrescar");
 
-function actualizarTabla(resultados) {
-  let tableHTML = "";
+function actualizarDiv(resultados) {
+  let divHTML = "";
   resultados.forEach((resultado) => {
     const hora = new Date(resultado.fecha).toLocaleTimeString('es-ES', {hour: '2-digit', minute: '2-digit'});
-    tableHTML += `
-      <tr>
-        <td>${resultado.equipoLocal}</td>
-        <td>${resultado.golesLocal}</td>
-        <td>${resultado.golesVisitante}</td>
-        <td>${resultado.equipoVisitante}</td>
-        <td>${hora}</td>
-      </tr>
+    const golesLocal = resultado.golesLocal;
+    const golesVisitante = resultado.golesVisitante;
+
+    divHTML += `
+      <div class="col-md-3 col-sm-12 mb-4">
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">${resultado.equipoLocal} vs ${resultado.equipoVisitante}</h5>
+            <div class="d-flex justify-content-around">
+              <div class="rounded resultado">
+                <p class="goles">${golesLocal}</p>
+                <p>GOLES</p>
+              </div>
+              <div class="rounded resultado">
+                <p class="goles">${golesVisitante}</p>
+                <p>GOLES</p>
+              </div>
+            </div>
+            <p class="card-text text-center">${hora}</p>
+          </div>
+        </div>
+      </div>
     `;
   });
-  resultadosTable.querySelector("tbody").innerHTML = tableHTML;
+  resultadosDiv.innerHTML = divHTML;
 }
 
 async function obtenerResultados() {
   try {
     const response = await fetch("http://localhost:8585/partidos");
     const resultados = await response.json();
-    actualizarTabla(resultados);
+    actualizarDiv(resultados);
   } catch (error) {
     console.error(error);
   }
